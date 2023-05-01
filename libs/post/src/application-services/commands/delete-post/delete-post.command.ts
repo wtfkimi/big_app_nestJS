fb0a3b53ce35cc3725b98fa2512482ec19d047d6
rdy2qa@gmail.com <rdy2qa@gmail.com>
2023-05-01 21:22:24 +0200

    ADD: create delete-post command

---
+++ libs/post/src/application-services/commands/delete-post/delete-post.command.ts
@@ -0,0 +1,6 @@
+
+
+export class DeletePostCommand {
+  constructor(public readonly id: string) {
+  }
+}
--- libs/post/src/application-services/commands/index.ts
+++ libs/post/src/application-services/commands/index.ts
@@ -6,10 +6,14 @@ import {
 import {
   UpdatePostCommandHandler
 } from "./update-post/update-post.command-handler";
+import {
+  DeletePostCommandHandler
+} from "./delete-post/delete-post.command-handler";
 
 
 
 export const POST_COMMANDS_HANDLERS: Type<ICommandHandler>[] = [
   CreatePostCommandHandler,
-  UpdatePostCommandHandler
+  UpdatePostCommandHandler,
+  DeletePostCommandHandler
 ];
