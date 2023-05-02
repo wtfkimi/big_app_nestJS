31365cc734b83688dfb3c5d1f27c4da6af4d03b3
rdy2qa@gmail.com <rdy2qa@gmail.com>
2023-05-02 14:25:49 +0200

    ADD: create get-all posts query

---
+++ libs/post/src/application-services/queries/get-posts/get-posts.query-handler.ts
@@ -0,0 +1,25 @@
+import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
+import { GetPostsQuery } from "./get-posts.query";
+import { PostAggregate } from "@lib/post";
+import { PostRepository } from "@lib/post/providers";
+import { Logger } from "@nestjs/common";
+
+
+QueryHandler(GetPostsQuery)
+export class GetPostsQueryHandler implements IQueryHandler<GetPostsQuery, [PostAggregate[], number]> {
+
+  private readonly logger = new Logger(GetPostsQueryHandler.name);
+
+  constructor(private readonly postRepository: PostRepository) {
+  }
+  async execute(query: GetPostsQuery): Promise<[PostAggregate[], number]> {
+
+    const [data, count] = await this.postRepository.findAll().catch(err => {
+      this.logger.error(err);
+
+      return [[], 0];
+    })
+    return [data, count] as [PostAggregate[], number];
+  }
+
+}
