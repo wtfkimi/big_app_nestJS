004cb7041beb375c0b436aae3df8993e48e77ac7
wtfkimi <rdy2qa@gmail.com>
2023-05-07 19:46:47 +0200

    UPD: update imports

--- libs/post/src/post.module.ts
+++ libs/post/src/post.module.ts
@@ -7,6 +7,8 @@ import { POST_QUERIES_HANDLERS } from "@lib/post/application-services/queries";
 import { POST_EVENTS_HANDLERS } from "@lib/post/application-services/events";
 import { PostFacade } from "@lib/post/application-services";
 import { postFacadeFactory } from "@lib/post/providers/post-facade.factory";
+import {PostRepository} from "@lib/post/providers";
+import {PostAdapter} from "@lib/post/providers/post.adapter";
 
 
 @Module({
@@ -19,6 +21,10 @@ import { postFacadeFactory } from "@lib/post/providers/post-facade.factory";
       provide: PostFacade,
       inject: [CommandBus, QueryBus, EventBus],
       useFactory: postFacadeFactory
+    },
+    {
+      provide: PostRepository,
+      useClass: PostAdapter,
     }
   ],
   exports: [PostFacade]
